a28a9fd4e6e29a3fe2d1c7736ae40d7c7c88dba7
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-20 11:46:36 -0800

    Add react-qr-scanner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { QrScanner } from '@yudiel/react-qr-scanner'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QrScanner
+      containerStyle={{ maxHeight: '100vh' }}
+      onDecode={(result) => console.log(result)}
+      onError={(error) => console.log(error?.message)}
+    />
+  )
 }
 
-export default App;
+export default App
