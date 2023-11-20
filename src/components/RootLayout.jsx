a4ec07e137649607c3350c90d9478b4d97e63e52
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-20 14:50:18 -0800

    Add appbar

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,17 +1,33 @@
 import { React, useState } from 'react'
-import Button from '@mui/material/Button'
 
 import { Outlet } from 'react-router-dom'
 
 import Navbar from './Navbar'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
 
 const Home = () => {
   const [isNavOpen, setIsNavOpen] = useState(false)
 
   return (
     <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => setIsNavOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <Navbar isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
-      <Button onClick={() => setIsNavOpen(true)}>Navigation</Button>
       <Outlet />
     </div>
   )
