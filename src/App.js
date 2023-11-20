84908a3ac0658bc962523bf19db0262e0bec4027
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-20 14:35:07 -0800

    Rename Home to RootLayout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { React } from 'react'
 import QrScannerPage from './components/QrScannerPage'
 import Character from './components/Character'
 import Quests from './components/Quests'
-import Home from './components/Home'
+import RootLayout from './components/RootLayout'
 
 import { createBrowserRouter, RouterProvider, redirect } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <RootLayout />,
     children: [
       { index: true, loader: () => redirect('/character') },
       {
