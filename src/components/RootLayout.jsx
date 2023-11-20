bd7d87827080a527017eea79f6e7825c81747a77
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-20 15:21:43 -0800

    Add new top bar

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -7,27 +7,51 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
+import Grid from '@mui/material/Grid'
+
+import { styled } from '@mui/material/styles'
+import LinearProgress, {
+  linearProgressClasses,
+} from '@mui/material/LinearProgress'
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: '#50C878',
+  },
+}))
 
 const Home = () => {
   const [isNavOpen, setIsNavOpen] = useState(false)
 
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => setIsNavOpen(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Navbar isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
+      <div
+        style={{
+          backgroundColor: '#36454F',
+          paddingBottom: '1rem',
+          boxShadow: '0px 2px 2px gray',
+        }}
+      >
+        <Navbar isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
+        <div style={{ textAlign: 'center' }}>
+          <h1 style={{ marginBottom: '0', marginTop: '0', color: 'white' }}>
+            Jefficus
+          </h1>
+          <h2 style={{ marginTop: '0', color: 'white' }}>Level 1</h2>
+          <BorderLinearProgress
+            style={{ marginLeft: '1rem', marginRight: '1rem' }}
+            variant="determinate"
+            value={50}
+          />
+        </div>
+      </div>
       <Outlet />
     </div>
   )
