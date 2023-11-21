43e1f1870c7b72cb96552f37398f297096ce492d
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-20 16:31:50 -0800

    Implement bottom layout

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -3,16 +3,19 @@ import { React, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import Navbar from './Navbar'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import BottomNavigation from '@mui/material/BottomNavigation'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
+import PersonIcon from '@mui/icons-material/Person'
+import CameraAltIcon from '@mui/icons-material/CameraAlt'
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer'
 
 import { styled } from '@mui/material/styles'
 import LinearProgress, {
   linearProgressClasses,
 } from '@mui/material/LinearProgress'
+
+import { useNavigate } from 'react-router-dom'
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 10,
@@ -27,8 +30,16 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   },
 }))
 
+const NAV_LINKS = {
+  Character: { route: 'character', icon: <PersonIcon /> },
+  'QR Scanner': { route: 'qr-scanner', icon: <CameraAltIcon /> },
+  Quests: { route: 'quests', icon: <QuestionAnswerIcon /> },
+}
+
 const Home = () => {
   const [isNavOpen, setIsNavOpen] = useState(false)
+  const [value, setValue] = useState(0)
+  const navigate = useNavigate()
 
   return (
     <div>
@@ -53,6 +64,27 @@ const Home = () => {
         </div>
       </div>
       <Outlet />
+      <Paper
+        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue)
+            console.log(
+              'Object.entries(NAV_LINKS)[newValue][1].route',
+              Object.entries(NAV_LINKS)[newValue][1].route
+            )
+            navigate('/' + Object.entries(NAV_LINKS)[newValue][1].route)
+          }}
+        >
+          {Object.entries(NAV_LINKS).map(([key, value]) => (
+            <BottomNavigationAction key={key} label={key} icon={value.icon} />
+          ))}
+        </BottomNavigation>
+      </Paper>
     </div>
   )
 }
