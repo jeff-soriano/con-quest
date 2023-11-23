67409e7550c2a197c3768dd4c83844f5e91f08f9
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-22 20:36:56 -0800

    Add basic list to quests page

--- src/components/Quests.jsx
+++ src/components/Quests.jsx
@@ -2,6 +2,13 @@ import { React } from 'react'
 import { useOutletContext } from 'react-router-dom'
 
 import Button from '@mui/material/Button'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemButton from '@mui/material/ListItemButton'
+import FolderIcon from '@mui/icons-material/Folder'
+import Avatar from '@mui/material/Avatar'
 
 const Quests = () => {
   const [setExp] = useOutletContext()
@@ -9,9 +16,27 @@ const Quests = () => {
   const addExp = () => setExp((xp) => (xp < 100 ? xp + 5 : xp))
 
   return (
-    <div>
-      <h1>Quests page!</h1>
-      <Button onClick={addExp} variant="contained">
+    <div style={{ textAlign: 'center' }}>
+      <List disablePadding>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemAvatar>
+              <Avatar>
+                <FolderIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary="Survey on your last visit"
+              secondary="Complete this survey for more exp!"
+            />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Button
+        onClick={addExp}
+        variant="contained"
+        style={{ marginTop: '20px' }}
+      >
         Add experience
       </Button>
     </div>
